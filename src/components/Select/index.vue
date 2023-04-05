e77293b17f0d780711d877685d411f7afb228ace
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2023-04-05 20:41:17 +0700

    feat: styling select in vee validate 🎨

--- src/components/Select/index.vue
+++ src/components/Select/index.vue
@@ -1,14 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { PropType } from "vue";
+import { ISelect } from "./select.type";
+import { Field, ErrorMessage } from "vee-validate";
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    default: "",
+  },
+  id: {
+    type: String,
+    default: null,
+  },
+  data: {
+    type: Array as PropType<Array<ISelect>>,
+    required: true,
+  },
+});
+</script>
 
 <template>
   <div class="select">
-    <label>Select Country</label>
-    <select name="cars" id="cars" placeholder="Choose tour country" value="">
-      <option value="volvo">Volvo</option>
-      <option value="saab">Saab</option>
-      <option value="mercedes">Mercedes</option>
-      <option value="audi">Audi</option>
-    </select>
+    <Field v-slot="{ field, errors }" :label="label" :name="name">
+      <div class="select-wrapper">
+        <label>{{ label }}</label>
+        <select v-bind="field" :name="name" :id="id">
+          <option value="" data-default hidden>Choose your country</option>
+          <option v-for="item in data" :key="item.value" :value="item.value">
+            {{ item.label }}
+          </option>
+        </select>
+      </div>
+
+      <ErrorMessage v-slot="{ message }" :name="name">
+        <div class="error">
+          <div class="error-message">{{ message }}</div>
+        </div>
+      </ErrorMessage>
+    </Field>
   </div>
 </template>
 
--- src/views/auth/register/index.vue
+++ src/views/auth/register/index.vue
@@ -1,31 +1,48 @@
 <script setup lang="ts">
 import TextInput from "@/components/Input";
 import Select from "@/components/Select";
+import Button from "@/components/Button";
+import country from "@/constant/country";
+import { Form, FormContext } from "vee-validate";
+import { ref } from "vue";
+
+const form = ref<FormContext>();
+
+const onRegister = async (value: Record<string, unknown>): Promise<void> => {
+  console.log(value);
+};
 </script>
 
 <template>
-  <div class="register">
-    <div class="register-title">
-      <h1>Create New Account</h1>
-      <p>Before you can join here, please create new account</p>
+  <Form @submit="onRegister" ref="form" v-slot="{ isSubmitting }">
+    <!-- :validation-schema="schema" -->
+    <div class="register">
+      <div class="register-title">
+        <h1>Create New Account</h1>
+        <p>Before you can join here, please create new account</p>
+      </div>
+      <p class="register-subtitle">Account Detail</p>
+      <div class="register-form">
+        <Select :data="country" label="Select Country" name="country" />
+        <TextInput
+          type="tel"
+          label="Phone Number"
+          name="phone_number"
+          placeholder="ex: 085725637261"
+        />
+        <TextInput
+          type="password"
+          label="Password"
+          name="password"
+          placeholder="Password"
+        />
+        <Button type="submit" variant="privy" class="mt-7 mb-2"
+          >Register</Button
+        >
+        <Button type="reset" variant="reset" outlined>Reset</Button>
+      </div>
     </div>
-    <p class="register-subtitle">Account Detail</p>
-    <div class="register-form">
-      <Select />
-      <TextInput
-        type="tel"
-        label="Phone Number"
-        name="phone_number"
-        placeholder="ex: 085725637261"
-      />
-      <TextInput
-        type="password"
-        label="Password"
-        name="password"
-        placeholder="Password"
-      />
-    </div>
-  </div>
+  </Form>
 </template>
 
 <style src="./register.style.scss" lang="scss"></style>
